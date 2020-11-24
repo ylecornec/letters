f072350c3ca758edcd8d0ee763d5290210b3ec42
Le Cornec Yves-Stan <yves.stan.lecornec@gmail.com>
2020-11-24 17:59:01 +0100

    check environment variables at launch

--- words/launch.sh
+++ words/launch.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
+if [ -z "$DB_PASSWORD" ]
+then
+	echo "environment variable DB_PASSWORD must be set"
+	exit 1
+fi
+
+if [ -z "$DB_HOST" ]
+then
+	echo "environment variable DB_HOST must be set"
+	exit 1
+fi
+
+if [ -z "$DB_PORT" ]
+then
+	echo "environment variable DB_PORT must be set"
+	exit 1
+fi
+
+if [ -z "$DB_NAME" ]
+then
+	echo "environment variable DB_NAME must be set"
+	exit 1
+fi
 eval $(opam env) && make test.opt
